1e6e962fb1c97288fd378bd63fc14ea773ec076a
sKovanskiys <jjjorik1997@gmail.com>
2023-06-16 12:47:52 +0500

    baxti's first commit

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import requests
-
-country = 'usa'
-operator = 'any'
-
-headers = {
-    'Accept': 'application/json',
-}
-
-response = requests.get('https://5sim.net/v1/guest/products/' + country + '/' + operator, headers=headers)
-req_result = response.json()
-print(req_result['openai']['Price'])
-#john's branch
+#baxti cort
