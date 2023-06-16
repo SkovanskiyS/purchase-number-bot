078be41d3f2729dae502252bd560622c6e1b31ff
sKovanskiys <jjjorik1997@gmail.com>
2023-06-16 12:38:22 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+country = 'usa'
+operator = 'any'
+
+headers = {
+    'Accept': 'application/json',
+}
+
+response = requests.get('https://5sim.net/v1/guest/products/' + country + '/' + operator, headers=headers)
+req_result = response.json()
+print(req_result['openai']['Price'])
