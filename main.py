53366f54892ac0b86b24a30714fff3db4cf54b05
sKovanskiys <jjjorik1997@gmail.com>
2023-06-18 00:46:34 +0500

    nothing appropriate

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.dispatcher.filters import Command
-import logging
+# from aiogram import Bot, Dispatcher, executor, types
+# from aiogram.dispatcher.filters import Command
+# import logging
+#
+# API_TOKEN = '1355012700:AAGZAeq9HihUdmwhbb_R-y-uMMgu4N0PJg4'
+# logging.basicConfig(level=logging.INFO)
+#
+# bot = Bot(token=API_TOKEN)
+# dp = Dispatcher(bot)
+#
+#
+# @dp.message_handler(Command('help'))
+# async def echo(message: types.Message):
+#     await message.answer(message.text)
+#
+#
+# @dp.message_handler(Command('start'))
+# async def replier(message: types.Message):
+#     await message.reply('My name is chiki')
+#
+#
+# executor.start_polling(dp, skip_updates=True)
 
-API_TOKEN = '1355012700:AAGZAeq9HihUdmwhbb_R-y-uMMgu4N0PJg4'
-logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+class Teacher:
+    degree = 'High degree'
+    unversity = 'Amity'
 
-
-@dp.message_handler(Command('help'))
-async def echo(message: types.Message):
-    await message.answer(message.text)
+    def teach(self):
+        print('i am teaching')
 
 
-@dp.message_handler(Command('start'))
-async def replier(message: types.Message):
-    await message.reply('My name is chiki')
-
-
-executor.start_polling(dp, skip_updates=True)
+parveen = Teacher()
+jahongir = Teacher()
+print(parveen.degree)
+parveen.teach()
